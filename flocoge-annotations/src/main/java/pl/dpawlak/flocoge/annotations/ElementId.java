3caa885bca8b67dc14976ef147e8ff3e6ea9d587
Dominik Pawlak <domel8dp@yahoo.co.uk>
2014-12-14 22:21:12 +0100

    Create skeleton for the project
    Create main and annotation projects, use Gradle as the build tool.

---
+++ flocoge-annotations/src/main/java/pl/dpawlak/flocoge/annotations/ElementId.java
@@ -0,0 +1,9 @@
+package pl.dpawlak.flocoge.annotations;
+
+/**
+ * Created by dpawlak on Dec 14, 2014
+ */
+public @interface ElementId {
+
+    int value();
+}
---
+++ flocoge/src/main/java/pl/dpawlak/flocoge/Main.java
@@ -0,0 +1,9 @@
+package pl.dpawlak.flocoge;
+
+/**
+ * Created by dpawlak on Dec 14, 2014
+ */
+public class Main {
+
+    public static void main(String[] args) { }
+}
