14e2328e6a5da3add3518c67be8177ee4d99f5d9
Dominik Pawlak <domel8dp@yahoo.co.uk>
2015-12-17 22:53:10 +0100

    Refine transformed model printer

--- flocoge/src/main/java/pl/dpawlak/flocoge/log/ModelPrinter.java
+++ flocoge/src/main/java/pl/dpawlak/flocoge/log/ModelPrinter.java
@@ -2,6 +2,7 @@ package pl.dpawlak.flocoge.log;
 
 import java.util.Map;
 
+import pl.dpawlak.flocoge.model.DecisionMeta;
 import pl.dpawlak.flocoge.model.FlocogeModel;
 import pl.dpawlak.flocoge.model.ModelConnection;
 import pl.dpawlak.flocoge.model.ModelElement;
@@ -9,30 +10,52 @@ import pl.dpawlak.flocoge.model.ModelElement.Shape;
 
 public class ModelPrinter {
 
+    private FlocogeModel model;
+
     public void print(FlocogeModel model) {
-        System.out.println("Diagram model:");
+        this.model = model;
+        System.out.println("==============");
+        System.out.println("Transformed model:");
         for (Map.Entry<String, ModelElement> path : model.startElements.entrySet()) {
-            System.out.println("START PATH: " + path.getKey());
-            traverseBranch(path.getValue(), 1);
+            printPath(path);
+            traverseBranch(path.getValue(), 1, null);
         }
+        System.out.println("==============");
     }
 
-    private void traverseBranch(ModelElement startElement, int level) {
+    private void printPath(Map.Entry<String, ModelElement> path) {
+        switch (path.getValue().shape) {
+            case ON_PAGE_REF:
+                System.out.print("private: ");
+                break;
+            case OFF_PAGE_REF:
+                System.out.print("public external: ");
+                break;
+            default:
+                System.out.print("public: ");
+                break;
+        }
+        System.out.println(path.getKey());
+    }
+
+    private ModelElement traverseBranch(ModelElement startElement, int level, String mergePoint) {
         ModelElement element = startElement;
-        while (element != null) {
-            indent(level);
-            printElement(element);
+        while (element != null && (mergePoint == null || !element.id.equals(mergePoint))) {
+            printElement(element, level);
             if (element.shape == Shape.DECISION) {
-                for (ModelConnection connection : element.connections) {
-                    indent(level);
-                    System.out.println("  BRANCH: " + connection.label);
-                    traverseBranch(connection.target, level + 1);
-                }
-                element = null;
+                element = traverseDecisionBranches(element, level + 1, mergePoint);
             } else {
                 element = getNextElement(element);
             }
         }
+        return element;
+    }
+
+    private void printElement(ModelElement element, int level) {
+        indent(level);
+        System.out.print(element.shape.name().toLowerCase());
+        System.out.print(": ");
+        System.out.println(element.label);
     }
 
     private void indent(int level) {
@@ -41,16 +64,37 @@ public class ModelPrinter {
         }
     }
 
-    private void printElement(ModelElement element) {
-        System.out.print(element.shape.name());
-        System.out.print(", label: ");
-        System.out.print(element.label);
-        System.out.print(", id: ");
-        System.out.println(element.id);
+    private ModelElement traverseDecisionBranches(ModelElement element, int level, String currentMergePoint) {
+        DecisionMeta meta = model.decisions.get(element.id);
+        int index = 0;
+        ModelElement finalElement = null;
+        for (ModelConnection connection : element.connections) {
+            String branchMergePoint = meta.mergePoints[index++];
+            printBranch(connection, branchMergePoint, level);
+            String mergePoint = branchMergePoint != null ? branchMergePoint : currentMergePoint;
+            ModelElement branchFinalElement = traverseBranch(connection.target, level + 1, mergePoint);
+            if (finalElement == null && (mergePoint != null || meta.hasMergePoints())) {
+                printReturn(level + 1);
+            }
+            if (branchFinalElement != null && finalElement == null) {
+                finalElement = branchFinalElement;
+            }
+        }
+        return finalElement;
+    }
+
+    private void printBranch(ModelConnection connection, String mergePoint, int level) {
+        indent(level);
+        System.out.print("branch: ");
+        System.out.println(connection.label);
+    }
+
+    private void printReturn(int level) {
+        indent(level);
+        System.out.println("*exit*");
     }
 
     private ModelElement getNextElement(ModelElement element) {
-        int connectionsCount = element.connections.size();
-        return connectionsCount > 0 ? element.connections.get(0).target : null;
+        return element.connections.isEmpty() ? null : element.connections.get(0).target;
     }
 }
